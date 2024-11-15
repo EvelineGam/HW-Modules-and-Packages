ed8441c900854ad967989eaadda372b955816b40
EvelineGam <lolitasmile19@gmail.com>
2024-11-15 13:58:04 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_math import devide as fm_devide
+from true_math import devide as tm_devide
+res1 = fm_devide(69,3)
+res2 = fm_devide(3,0)
+res3 = tm_devide(49,7)
+res4 = tm_devide(15,0)
+print(res1)
+print(res2)
+print(res3)
+print(res4)
---
+++ true_math.py
@@ -0,0 +1,5 @@
+def devide(first,second):
+    from math import inf
+    if second == 0:
+        return inf
+    return first / second
